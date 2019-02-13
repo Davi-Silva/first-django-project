ceafd48489d41b79ecf9c6bee62a904c7f326a25
Davi Cunha <davi.cunha.silva.ca@gmail.com>
2019-02-13 04:13:11 -0200

    A lot of stuff added.

--- courses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Courses, OldCourses
-
-# Register your models here.
-
-admin.site.register(Courses)
-admin.site.register(OldCourses)
--- first_project/settings.py
+++ first_project/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
     # own
     'products',
-    'courses',
     'pages',
 
 ]
@@ -63,7 +62,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
