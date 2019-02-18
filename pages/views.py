f74c53d9447980a2c506ac9cf5dd682734a2ff87
Davi Cunha <davi.cunha.silva.ca@gmail.com>
2019-02-18 01:43:05 -0300

    Templates django files updated.

--- pages/views.py
+++ pages/views.py
@@ -6,22 +6,23 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>Hello World!</h1>")
-    return render(request, "home.html")
+    return render(request, "home.html", {})
 
 
 def contact_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>Contact Page!</h1>")
-    return render(request, "contact.html")
+    return render(request, "contact.html", {})
 
 
 def about_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>About Page!</h1>")
-    return render(request, "about.html")
+    my_context = {
+        "my_text" : "This a about us",
+        "my_number" : 123,
+        "my_list" : [123, 4242, [987, 654], "ABC", "Davi", "This a website in development", 312]
+    }
+    return render(request, "about.html", my_context)
 
 
 def social_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>Social Page!</h1>")
-    return render(request, "social.html")
+    return render(request, "social.html", {})
 
 
