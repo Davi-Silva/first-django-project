2c7e3a2a23b9627b3a725691531d6cc5a1c25f7e
Davi Cunha <davi.cunha.silva.ca@gmail.com>
2019-02-18 16:26:37 -0300

    Some modifications made.

--- products/models.py
+++ products/models.py
@@ -8,6 +8,6 @@ class Product(models.Model):
     price       = models.DecimalField(decimal_places=2, max_digits=1000, default=0.00)
     # blank=True means that this field is required to submuit the form to the DB / null=True means that the following column in the DB can be empty.
     summary     = models.TextField(blank=True, null=True) 
-    feature     = models.BooleanField() #null=True, default=True
+    feature     = models.BooleanField(default=True) #null=True, default=True
 
 
--- products/views.py
+++ products/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
 
 
 # Create your views here.
 def product_detail_view(request):
-    obj = Product.objects.get(id=1)
+    obj = Product.objects.get(id=10)
     my_context = {
         'object': obj
     }
-    return render(request, "product/detail.html", my_context)
+    return render(request, "products/product_detail.html", my_context)
+
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+
+    my_context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", my_context)
 
