10d853c4363965e4d20b5fa5abd2113f6e39e82a
Davi Cunha <davi.cunha.silva.ca@gmail.com>
2019-02-18 21:39:37 -0300

    New ways to validate forms.

--- products/forms.py
+++ products/forms.py
@@ -2,14 +2,63 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title       = forms.CharField(
+        label = '',
+        widget=forms.TextInput(
+            attrs={
+                "placeholder" : "Title",
+                "class" : "title one",
+                "id" : "title",
+            }
+        )
+    )
+
+    email       = forms.EmailField(
+        label = '',
+        widget=forms.EmailInput(
+            attrs={
+                "class" : "email",
+                "id" : "email",
+                "placeholder" : "E-mail"
+            }
+        )
+    )
+
+    description = forms.CharField(
+        label = '',
+        widget=forms.Textarea(
+            attrs={
+                "placeholder" : "Description",
+                "class" : "product-description",
+                "id" : "description",
+                "rows" : 10,
+                "cols" : 30
+            }
+        )
+    )
+
+    price       = forms.DecimalField(initial = 0.0, label = '')
+
     class Meta:
         model = Product
         fields = [
             'title',
             'description',
-            'price', 
-            'summary'
+            'price'
         ]
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if not "title" in title.lower():
+            raise forms.ValidationError("This is not a valid title")
+        return title
+
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get("email")
+        if not email.endswith("edu"):
+            raise forms.ValidationError("This is not a valid title")
+        return email
 
 
 class RawProductForm(forms.Form):
--- products/views.py
+++ products/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .models import Product
 from .forms import ProductForm, RawProductForm
 
-
 # Create your views here.
 def product_detail_view(request):
     obj = Product.objects.get(id=10)
@@ -12,16 +11,16 @@ def product_detail_view(request):
     return render(request, "products/product_detail.html", my_context)
 
 # Create a form view
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
 
-#     my_context = {
-#         'form': form
-#     }
-#     return render(request, "products/product_create.html", my_context)
+    my_context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", my_context)
 
 # def product_create_view(request):
 #     # print(request.POST)
@@ -33,19 +32,20 @@ def product_detail_view(request):
 #     my_context = {}
 #     return render(request, "products/product_create.html", my_context)
 
-def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == "POST":
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            # new the data is good
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
-    my_context = {
-        "form": my_form
-    }
-    return render(request, "products/product_create.html", my_context)
+# def product_create_view(request):
+#     my_form = RawProductForm()
+#     if request.method == "POST":
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             # new the data is good
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
+#     my_context = {
+#         "form": my_form
+#     }
+#     return render(request, "products/product_create.html", my_context)
+
 
 
